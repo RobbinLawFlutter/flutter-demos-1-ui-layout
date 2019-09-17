e92abe265d6316d2730a97088b6737acb204005d
Robbin <robbinl@telusplanet.net>
2019-09-17 16:04:41 -0600

    a

---
+++ ad_container_widget/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+// display an asset image
+// drag and drop an image to appicon.co then generate a zip file
+// in studio navigate to android/app/src/main/res
+void main() {
+  runApp(
+    myApp(),
+  );
+}
+
+class myApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      //first level widget of Material Design
+      home: Scaffold(
+        //default route
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text("Asset Image & Launch Icon"),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(image: AssetImage('images/bottle.jpg')),
+        ),
+      ),
+    );
+  }
+}
