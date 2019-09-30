d7af0f95a99af1767a1e6c82a2ad9c4ade0334f6
Robbin <robbinl@telusplanet.net>
2019-09-30 15:37:34 -0600

    a

---
+++ aq-custom-widgets/lib/input.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+class MyFirstPage extends StatefulWidget {
+  @override
+  _MyFirstPageState createState() => _MyFirstPageState();
+}
+
+class _MyFirstPageState extends State<MyFirstPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Calculator'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ReuseableCard(
+                    myColor: Color(0xFF1D1E33),
+                  ),
+                ),
+                Expanded(
+                  child: ReuseableCard(
+                    myColor: Color(0xFF1D1E33),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ReuseableCard(
+              myColor: Color(0xFF1D1E33),
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ReuseableCard(
+                    myColor: Color(0xFF1D1E33),
+                  ),
+                ),
+                Expanded(
+                  child: ReuseableCard(
+                    myColor: Color(0xFF1D1E33),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ReuseableCard extends StatelessWidget {
+  ReuseableCard(
+      {@required this.myColor}); // constructor that requires a Color parameter.
+
+  final Color
+      myColor; // final makes this property called myColor of type Color immutable, cannot change once set.
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15),
+      decoration: BoxDecoration(
+        color: myColor,
+        borderRadius: BorderRadius.circular(10),
+      ),
+    );
+  }
+}
--- aq-custom-widgets/lib/main.dart
+++ aq-custom-widgets/lib/main.dart
@@ -1,6 +1,9 @@
 // this app shows themes
+// go to dribbble.com and search for simple bmi
+// go to colorzilla.com and download the color picker
 
 import 'package:flutter/material.dart';
+import 'input.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,23 +11,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MyFirstPage(),
-    );
-  }
-}
-
-class MyFirstPage extends StatefulWidget {
-  @override
-  _MyFirstPageState createState() => _MyFirstPageState();
-}
-
-class _MyFirstPageState extends State<MyFirstPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Calculator'),
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Color(0xFF0B1034),
+        // the 0xFF is the transparency, then 0B Red, 10 Green, 34 Blue
+        scaffoldBackgroundColor: Color(0xFF0B1034),
       ),
+      home: MyFirstPage(),
     );
   }
 }
