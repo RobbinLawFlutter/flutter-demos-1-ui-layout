e2f1fda87c881c5b513eac75f2c2496091bee38a
Robbin <robbinl@telusplanet.net>
2019-10-03 09:51:32 -0600

    a

--- aq-custom-widgets/lib/input.dart
+++ aq-custom-widgets/lib/input.dart
@@ -1,6 +1,12 @@
 // use the color picker again to pick the color of the ReuseableCard
+// go to pub.dev and search for font_awesome_flutter package and download and configure pubspec.yaml
 
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+const double bottomContainerHeight = 80;
+const activeCardColor = Color(0xFF1D1E33);
+const bottomContainerColor = Color(0xFFEB1555);
 
 class MyFirstPage extends StatefulWidget {
   @override
@@ -21,12 +27,28 @@ class _MyFirstPageState extends State<MyFirstPage> {
               children: <Widget>[
                 Expanded(
                   child: ReuseableCard(
-                    myColor: Color(0xFF1D1E33),
+                    myColor: activeCardColor,
+                    reusableCardChild: Column(
+                      children: <Widget>[
+                        Icon(
+                          FontAwesomeIcons.mars,
+                          size: 80,
+                        ),
+                        SizedBox(
+                          height: 15,
+                        ),
+                        Text(
+                          'MALE',
+                          style:
+                              TextStyle(fontSize: 18, color: Color(0xFF8D8E98)),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
                 Expanded(
                   child: ReuseableCard(
-                    myColor: Color(0xFF1D1E33),
+                    myColor: activeCardColor,
                   ),
                 ),
               ],
@@ -34,7 +56,7 @@ class _MyFirstPageState extends State<MyFirstPage> {
           ),
           Expanded(
             child: ReuseableCard(
-              myColor: Color(0xFF1D1E33),
+              myColor: activeCardColor,
             ),
           ),
           Expanded(
@@ -42,16 +64,22 @@ class _MyFirstPageState extends State<MyFirstPage> {
               children: <Widget>[
                 Expanded(
                   child: ReuseableCard(
-                    myColor: Color(0xFF1D1E33),
+                    myColor: activeCardColor,
                   ),
                 ),
                 Expanded(
                   child: ReuseableCard(
-                    myColor: Color(0xFF1D1E33),
+                    myColor: activeCardColor,
                   ),
                 ),
               ],
             ),
+          ),
+          Container(
+            color: bottomContainerColor,
+            margin: EdgeInsets.only(top: 10),
+            width: double.infinity,
+            height: bottomContainerHeight,
           ),
         ],
       ),
@@ -61,14 +89,18 @@ class _MyFirstPageState extends State<MyFirstPage> {
 
 class ReuseableCard extends StatelessWidget {
   ReuseableCard(
-      {@required this.myColor}); // constructor that requires a Color parameter.
+      {@required this.myColor,
+      this.reusableCardChild}); // constructor that requires a Color parameter.
 
   final Color
       myColor; // final makes this property called myColor of type Color immutable, cannot change once set.
 
+  final Widget reusableCardChild;
+
   @override
   Widget build(BuildContext context) {
     return Container(
+      child: reusableCardChild,
       margin: EdgeInsets.all(15),
       decoration: BoxDecoration(
         color: myColor,
