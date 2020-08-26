44e97ecfe5e89b6934042fcf230576abe4208a97
robbinl <robbinl@telusplanet.net>
2020-08-26 16:49:44 -0600

    a

--- aac_first_program/lib/main.dart
+++ aac_first_program/lib/main.dart
@@ -10,7 +10,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
       home: Scaffold(
         appBar: AppBar(
           title: Text('Welcome to Flutter'),
