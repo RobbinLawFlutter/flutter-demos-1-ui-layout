be4af9bee0f81a818a8083ea7651bba928e9cda1
robbinl <robbinl@telusplanet.net>
2020-09-14 14:59:32 -0600

    a

--- aac_first_program/lib/main.dart
+++ aac_first_program/lib/main.dart
@@ -1,3 +1,12 @@
+//In this app we create our own class called MyApp.
+//runApp will instanciate this MyApp into an Widget object
+//and run it.
+//The extends key word means that we are inheriting
+//from the StatelessWidget class.
+//The @override keyword means that we are changing
+//the base classes build method to our own.
+//Our version will return our produced MaterialApp Widget tree.
+
 // https://www.youtube.com/watch?v=wE7khGHVkYY
 
 // https://flutter.dev/docs/development/ui/layout
@@ -13,9 +22,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.blueGrey,
         appBar: AppBar(
-          backgroundColor: Colors.blueGrey[900],
           title: Text('Welcome to Flutter'),
         ),
         body: Center(
