85a785245440c085f1de5b27cc4a9215c52a7b0b
robbinl <robbinl@telusplanet.net>
2020-09-14 09:37:24 -0600

    a

--- aaa_first_program/lib/main.dart
+++ aaa_first_program/lib/main.dart
@@ -1,12 +1,14 @@
 // Define a function.
 printInteger(int aNumber) {
-  print(
-      'The number is $aNumber.'); // Print to console using string interpolation.
+  // Print to console using string interpolation.
+  print('The number is $aNumber.');
 }
 
 // This is where the app starts executing.
 main() {
   print('This is our first Dart App');
-  var number = 42; // Declare and initialize a variable.
-  printInteger(number); // Call a function.
+  // Declare and initialize a variable.
+  var number = 42;
+  // Call a function.
+  printInteger(number);
 }
