f71ae2a637214f0395f0f279440e2fa8a05b631e
robbinl <robbinl@telusplanet.net>
2020-09-27 15:01:35 -0600

    a

---
+++ aob_dart_oop/lib/question.dart
@@ -0,0 +1,20 @@
+class Question {
+  String questionText;
+  bool questionAnswer;
+
+  Question(String q, bool a) {
+    questionText = q;
+    questionAnswer = a;
+  }
+
+/*
+  Question(String q, bool a) {
+    this.questionText = q;
+    this.questionAnswer = a;
+  }
+*/
+
+/*
+  Question(this.questionText, this.questionAnswer);
+*/
+}
