e6dbdd20919e42c40d29564d58b4441d563461e3
robbinl <robbinl@telusplanet.net>
2020-10-01 12:08:09 -0600

    a

--- aqc-navigation-BMI/lib/main.dart
+++ aqc-navigation-BMI/lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData.dark().copyWith(
         primaryColor: Color(0xFF0B1034),
-        // the 0xFF is the transparency, then 0B Red, 10 Green, 34 Blue
+        // the 0x means HEX the FF is the transparency, then 0B Red, 10 Green, 34 Blue
         scaffoldBackgroundColor: Color(0xFF0B1034),
       ),
       home: MySeventhPage(),
---
+++ as-named-routes-navigation/lib/screen1.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'screen2.dart';
+
+class Screen1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: Text('Screen 1'),
+      ),
+      body: Center(
+        child: RaisedButton(
+          color: Colors.blue,
+          child: Text('Go To Screen 2'),
+          onPressed: () {
+            Navigator.pushNamed(
+              context,
+              '/second',
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
