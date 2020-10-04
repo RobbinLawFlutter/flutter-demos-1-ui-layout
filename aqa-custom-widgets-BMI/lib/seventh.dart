b3e9a3cfcaec3a8d7a4b826ce75f446d5144217b
robbinl <robbinl@telusplanet.net>
2020-10-04 11:44:58 -0600

    a

--- aqa-custom-widgets-BMI/lib/seventh.dart
+++ aqa-custom-widgets-BMI/lib/seventh.dart
@@ -6,6 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'reusable-card-enhanced.dart';
 import 'my-icon.dart';
+import 'round-icon-button.dart';
 import 'constants.dart';
 
 enum Gender {
@@ -222,28 +223,6 @@ class MySeventhPageState extends State<MySeventhPage> {
           ),
         ],
       ),
-    );
-  }
-}
-
-class RoundIconButton extends StatelessWidget {
-  RoundIconButton({@required this.icon, @required this.onPressed});
-
-  final IconData icon;
-  final Function onPressed;
-
-  @override
-  Widget build(BuildContext context) {
-    return RawMaterialButton(
-      child: Icon(icon),
-      onPressed: onPressed,
-      elevation: 6,
-      constraints: BoxConstraints.tightFor(
-        width: 56.0,
-        height: 56.0,
-      ),
-      shape: CircleBorder(),
-      fillColor: Color(0xFF4C4F5E),
     );
   }
 }
