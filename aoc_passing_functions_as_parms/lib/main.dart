71c24d6b6cf5a65805a24f27d36a1304cc9bb402
robbinl <robbinl@telusplanet.net>
2020-10-06 10:11:34 -0600

    a

---
+++ aoc_passing_functions_as_parms/lib/main.dart
@@ -0,0 +1,12 @@
+void main() {
+  int result = add(3, 5);
+  print('The add result is $result');
+}
+
+int add(int n1, int n2) {
+  return (n1 + n2);
+}
+
+int multipy(int n1, int n2) {
+  return (n1 * n2);
+}
