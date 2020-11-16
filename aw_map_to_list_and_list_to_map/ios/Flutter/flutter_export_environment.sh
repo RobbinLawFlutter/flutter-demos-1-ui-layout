a83f5e6e5421d7949eb7df7d9285dfaabe9dac6d
robbinl <robbinl@telusplanet.net>
2020-11-16 09:58:43 -0700

    a

--- aw_map_to_list_and_list_to_map/ios/Flutter/flutter_export_environment.sh
+++ aw_map_to_list_and_list_to_map/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\FlutterDev\FlutterSDK\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\FlutterDev\MyAppRepos\flutter-dev\lab_2"
+export "FLUTTER_APPLICATION_PATH=C:\FlutterDev\MyAppRepos\flutter-begin\aw_map_to_list_and_list_to_map"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
--- aw_map_to_list_and_list_to_map/lib/main.dart
+++ aw_map_to_list_and_list_to_map/lib/main.dart
@@ -1,8 +1,7 @@
 class Customer {
+  Customer(this.name, this.age);
   String name;
   int age;
-
-  Customer(this.name, this.age);
 
   @override
   String toString() {
@@ -10,32 +9,53 @@ class Customer {
   }
 }
 
-main(List<String> args) {
+main() {
   Map map = {'Jack': 23, 'Adam': 27, 'Katherin': 25};
-
+  Map map0 = {'name': 'Jack', 'age': 27};
   var list1 = [];
-
+  //Convert a map to a list of Customer objects.
   map.forEach((k, v) => list1.add(Customer(k, v)));
-  print(list1);
+  print('list1= $list1');
+  //Another way to convert a map to a list of Customer objects.
+  var list2 = [];
+  map.entries.forEach((e) => list2.add(Customer(e.key, e.value)));
+  print('list2= $list2');
+  //Another way to convert a map to a list of Customer objects.
+  //We convert the map into a view and then to a list.
+  var list3 = [];
+  list3 = map.entries.map((e) => Customer(e.key, e.value)).toList();
+  print('list3= $list3');
 
-  map.entries.forEach((e) => list1.add(Customer(e.key, e.value)));
-  print(list1);
-
-  list1 = map.entries.map((e) => Customer(e.key, e.value)).toList();
-  print(list1);
-
-  List list2 = [];
-  list2.add(Customer('Jack', 23));
-  list2.add(Customer('Adam', 27));
-  list2.add(Customer('Katherin', 25));
-
-  var map1 = Map.fromIterable(list2, key: (e) => e.name, value: (e) => e.age);
-  print(map1);
-
+  //Creating a list of Customer objects.
+  List list4 = [];
+  list4.add(Customer('Jack', 23));
+  list4.add(Customer('Adam', 27));
+  list4.add(Customer('Katherin', 25));
+  //Converting a list to a map.
+  var map1 = Map.fromIterable(list4, key: (e) => e.name, value: (e) => e.age);
+  print('map1= $map1');
+  //Another way to convert a list to a map.
   var map2 = {};
-  list2.forEach((customer) => map2[customer.name] = customer.age);
-  print(map2);
+  list4.forEach((customer) => map2[customer.name] = customer.age);
+  print('map2= $map2');
+  //Creating a view from a list.
+  //Notice the data in this view is completely
+  //different from anything in the list.
+  var view1 = list4.map((e) => 'hey');
+  print('view1= $view1');
 
-  var map3 = list2.map((e) => 'hey');
-  print(map3);
+  //Creating a list of maps.
+  List<Map<String, dynamic>> myListOfMaps = [
+    {'name': 'Jack', 'age': 23},
+    {'name': 'Adam', 'age': 27},
+    {'name': 'katie', 'age': 25},
+  ];
+  // Convert the List<Map<String, dynamic> into a List<Customer>.
+  var list5 = List.generate(myListOfMaps.length, (i) {
+    return Customer(
+      myListOfMaps[i]['name'],
+      myListOfMaps[i]['age'],
+    );
+  });
+  print('list5= $list5');
 }
