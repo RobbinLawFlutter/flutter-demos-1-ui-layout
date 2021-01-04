22c070f8fba8a86c8fc334546f233869e283f752
robbinl <robbinl@telusplanet.net>
2021-01-04 15:48:31 -0700

    a

--- cc-stateless-widget/lib/main.dart
+++ cc-stateless-widget/lib/main.dart
@@ -1,1 +1,37 @@
+//In this app we create our own class called MyApp.
+//runApp will instanciate this MyApp into a Widget object
+//and run it.
+//The extends key word means that we are inheriting
+//from the StatelessWidget class.
+//The @override keyword means that we are changing
+//the base classes build method to our own.
+//Our version will return our produced MaterialApp Widget tree.
 
+//stateless Widget from Widgets 101 series
+// https://www.youtube.com/watch?v=wE7khGHVkYY
+
+// https://flutter.dev/docs/development/ui/layout
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Intro to Stateless Widget'),
+        ),
+        body: Center(
+          child: Text(
+            'Hey Man',
+          ),
+        ),
+      ),
+    );
+  }
+}
