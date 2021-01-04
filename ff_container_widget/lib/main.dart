439a3b0d1f5c00431de6ca8a540ac483c571a2e2
robbinl <robbinl@telusplanet.net>
2021-01-04 15:08:30 -0700

    container

--- ff_container_widget/lib/main.dart
+++ ff_container_widget/lib/main.dart
@@ -10,6 +10,8 @@ import 'package:flutter/material.dart';
 //Choose to open DevTools in the browser and click on Debug Paint to show
 //the results of the padding and margin demo.
 
+//By default a container sizes itself to its child size.
+
 //We can use the padding and margin properties of the Container Widget
 //to apply padding and margin to the Container.
 
@@ -30,22 +32,33 @@ class MyApp extends StatelessWidget {
           backgroundColor: Colors.blueGrey[900],
         ),
         body: Container(
+          //if you do not specify a height and width the container will size to its child.
           height: 400.0,
           width: 340.0,
+          //default color is the color of the containers parent, the scaffold.
+          //cannot provide both a color and decoration at the same time.
           //color: Colors.white,
           //padding: EdgeInsets.fromLTRB(50, 40, 10, 50),
           //padding: EdgeInsets.all(100.0),
           //margin: EdgeInsets.fromLTRB(50.0, 40.0, 20.0, 20.0),
           //margin: EdgeInsets.all(100.0),
           //margin: EdgeInsets.symmetric(vertical: 50.0, horizontal: 10.0),
+
+          //align the child within the container default is center.
+          //alignment: Alignment.bottomCenter,
+          //alignment: Alignment.centerRight,
+          //alignment: Alignment.topLeft,
+
+          //you can decorate the container to change its shape and
+          //give it a border and color.
           decoration: BoxDecoration(
-            shape: BoxShape.rectangle,
-            color: Colors.amber,
+            //change the shape of the container default is rectangle.
+            //shape: BoxShape.rectangle,
+            //you cannot have color in two places.
+            //color: Colors.amber,
             border: Border.all(color: Colors.black, width: 3),
           ),
-          child: Image(
-            image: AssetImage('images/bottle.jpg'),
-          ),
+          child: Image.asset('images/bottle.jpg'),
         ),
       ),
     );
