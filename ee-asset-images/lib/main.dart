38f0a91648ff605c7d5e1bfdd855258d944a8caa
robbinl <robbinl@telusplanet.net>
2021-01-04 16:04:32 -0700

    a

--- ee-asset-images/lib/main.dart
+++ ee-asset-images/lib/main.dart
@@ -1,1 +1,33 @@
+import 'package:flutter/material.dart';
 
+//This app shows how to display an asset image.
+//Also we will get a custom launcher icon in place.
+//To change the launcher icon from the flutter icon
+//drag and drop an image to appicon.co then generate a zip file
+//unzip downloaded file
+//in VSCode navigate to android/app/src/main/res
+//right mouse and show in explorer
+//delete mipmap-hdpi,-mdpi,-xhdpi,-xxhdpi,-xxxhdpi directories
+//and replace with the new ones unzipped
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text("Asset Image"),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image.asset('images/bottle.jpg'),
+        ),
+      ),
+    );
+  }
+}
