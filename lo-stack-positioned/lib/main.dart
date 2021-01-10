5acf46d456fbe38a6ea10d7a1c41f0294a8e6eb9
robbinl <robbinl@telusplanet.net>
2021-01-10 16:30:04 -0700

    add vids

--- lo-stack-positioned/lib/main.dart
+++ lo-stack-positioned/lib/main.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 
+//stack widget of the week by the flutter team
+//https://www.youtube.com/watch?v=liEGSeD3Zt8&list=PLjxrf2q8roU23XGwz3Km7sQZFTdB996iG&index=50
+
+//positioned widget of the week by the flutter team
+//https://www.youtube.com/watch?v=EgtPleVwxBQ&list=PLjxrf2q8roU23XGwz3Km7sQZFTdB996iG&index=27
+
 //stacked and positioned widgets by RetroStudio
 //https://www.youtube.com/watch?v=1qlgbNN0BaE&list=PLybADvIp2cxiVOEHi9ooCHP2tAAihHQPX&index=6
 
