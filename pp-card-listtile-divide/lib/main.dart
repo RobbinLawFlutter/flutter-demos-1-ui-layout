4e5e740052cab71cc2a6cff7685909d232d23fe8
robbinl <robbinl@telusplanet.net>
2021-01-13 16:54:14 -0700

    card

--- pp-card-listtile-divide/lib/main.dart
+++ pp-card-listtile-divide/lib/main.dart
@@ -1,13 +1,5 @@
 import 'package:flutter/material.dart';
 
-//using the Icon Widget
-//go to http://www.materialpalette.com
-
-//Icons from Material
-//https://material.io/resources/icons/?style=baseline
-
-//SizedBox Widget of the Week
-//https://www.youtube.com/watch?v=EHPu_DzRfqA
 void main() {
   runApp(
     MyApp(),
@@ -23,65 +15,42 @@ class MyApp extends StatelessWidget {
         //default route
         backgroundColor: Colors.blueGrey,
         appBar: AppBar(
-          title: Text("Icon Widget"),
+          title: Text("Card, ListTile, and Divider Widgets"),
           backgroundColor: Colors.blue,
         ),
-        body: Column(
-          mainAxisSize: MainAxisSize.max,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: CircleAvatar(
-                radius: 150,
-                backgroundColor: Colors.blue,
-                backgroundImage: AssetImage('images/bottle.jpg'),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                'bottle pic',
-                style: TextStyle(
-                  fontFamily: 'Chilanka-Regular',
-                  fontSize: 40,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 2.5,
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Container(
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.black, width: 8),
-                  color: Colors.white,
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(10),
-                  child: Row(
-                    children: <Widget>[
-                      Icon(
-                        Icons.add_shopping_cart,
-                        size: 50,
-                        color: Colors.blue.shade500,
-                      ),
-                      SizedBox(
-                        width: 20,
-                      ),
-                      Text(
-                        'go to this place',
-                        style: TextStyle(
-                          fontSize: 25,
-                          color: Colors.blue.shade300,
-                        ),
-                      ),
-                    ],
+        body: SizedBox(
+          height: 250,
+          child: Card(
+            child: Column(
+              children: [
+                ListTile(
+                  title: Text('1625 Main Street',
+                      style: TextStyle(fontWeight: FontWeight.w500)),
+                  subtitle: Text('My City, CA 99984'),
+                  leading: Icon(
+                    Icons.restaurant_menu,
+                    color: Colors.blue[500],
                   ),
                 ),
-              ),
+                Divider(),
+                ListTile(
+                  title: Text('(408) 555-1212',
+                      style: TextStyle(fontWeight: FontWeight.w500)),
+                  leading: Icon(
+                    Icons.contact_phone,
+                    color: Colors.blue[500],
+                  ),
+                ),
+                ListTile(
+                  title: Text('costa@example.com'),
+                  leading: Icon(
+                    Icons.contact_mail,
+                    color: Colors.blue[500],
+                  ),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
